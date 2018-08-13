afa220ab7a79a6b33f27c654da311f5f0523b311
Rico Zhang <ricozhang726@gmail.com>
2018-08-13 16:20:54 +1000

    fix docker builds

--- market_service/main.go
+++ market_service/main.go
@@ -15,18 +15,11 @@ func main() {
 
 	showversion := kingpin.Flag("version", "Show version information.").Short('v').Bool()
 	validatorUrl := kingpin.Flag("validator", "Validator Url.").Default("tcp://localhost:4040").Short('d').String()
-	configPath := kingpin.Flag("config", "Optional config file path.").Default("config.user.json").String()
 	kingpin.Parse()
 
 	if *showversion == true {
 		fmt.Printf("Version: %s\n", version)
 		os.Exit(0)
-	}
-
-	_, err := os.Stat(*configPath)
-	if err != nil {
-		fmt.Printf("Config file: %s not found.\n", *configPath)
-		os.Exit(1)
 	}
 	if validatorUrl == nil {
 		panic("no validator url is defined")
